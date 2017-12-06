b9f7ba795666e9b6b5ba9b9e81fe5057281daff8
Szymon Pyżalski <szymon@pythonista.net>
2017-12-06 15:39:02 +0100

    Version with two formats

--- converter.py
+++ converter.py
@@ -1,22 +1,71 @@
+import argparse
 import csv
 import json
 
-IN_FILE = 'MOCK_DATA.csv'
-OUT_FILE = 'MOCK_DATA.json'
 
 # 1. Wczytać dane z csv
 # 2. Przekonwertować dane na listę
 # 3. Zapisać w formacie JSON
 
-values = []
+class JsonFormat:
 
-with open (IN_FILE) as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        values.append(row) 
+    def __init__(self, **kwars):
+        pass
+
+    def parse(self, fileName):
+        with open(fileName, 'r') as fRead:
+            return json.load(fRead)
+
+    def output(self, lTab, fileName):
+        with open(fileName, 'w') as fWrite:
+            json.dump(lTab, fWrite)
 
 
-with open (OUT_FILE,'w') as o:
-    json.dump(values, o)
+class CsvFormat:
+
+    def __init__(self, **kwars):
+        pass
+
+    def parse(self, filename):
+        with open(filename, 'r') as fRead:
+            reader = csv.DictReader(fRead)
+            return list(reader)
+
+    def output(self, lTab, filename):
+        with open(filename, 'w') as fWrite:
+            writer = csv.DictWriter(fWrite, fieldnames=lTab[0].keys())
+            writer.writeheader()
+            writer.writerows(lTab)
 
 
+FORMATS = {
+    'json': JsonFormat,
+    'csv': CsvFormat,
+}
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Converts between dataformats'
+    )
+    parser.add_argument('infile')
+    parser.add_argument('outfile')
+    parser.add_argument(
+        '-i', '--input-format',
+        required=True,
+        choices=FORMATS.keys()
+    )
+    parser.add_argument(
+        '-o', '--output-format',
+        required=True,
+        choices=FORMATS.keys()
+    )
+    args = parser.parse_args()
+    inputFormat = args.input_format
+    outputFormat = args.output_format
+    
+    data_parser = FORMATS[inputFormat]()
+    formatter = FORMATS[outputFormat]()
+    parsedValue = data_parser.parse(args.infile)
+    if len(parsedValue) == 0:
+        parser.error('Conversion of empty data not supported')
+    formatter.output(parsedValue, args.outfile)
